20868b033774bfeb5d66d0e00fc391062a24db65
lifehutsolutions-web <lifehutsolutions@gmail.com>
2026-07-01 14:54:18 +0530

    Delete studio/builder directory

--- studio/builder/exporter.ts
+++
@@ -1,9 +0,0 @@
-import { Project } from '../types';
-import { RenderEngine } from '../library/RenderEngine';
-
-/**
- * Delegated to the single RenderEngine module for architecture freezing.
- */
-export function generateExportHtml(project: Project): string {
-  return RenderEngine.render(project, { isPreview: false });
-}
