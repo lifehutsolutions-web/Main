b3b70c24898eb24026e559b21bfc041e92b8d56a
lifehutsolutions-web <lifehutsolutions@gmail.com>
2026-07-10 17:01:27 +0000

    Add Capacitor Android support

---
+++ workspace/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lifehut.workspace',
+  appName: 'Lifehut Workspace',
+  webDir: 'dist'
+};
+
+export default config;
