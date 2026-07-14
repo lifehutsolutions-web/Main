68ec5e4ce01be3c9c50351b9f24dd548d09dda3f
lifehutsolutions-web <lifehutsolutions@gmail.com>
2026-07-14 12:20:48 +0530

    Logo updated

--- workspace/src/components/Logo.tsx
+++ workspace/src/components/Logo.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
+import logo from '../assets/branding/logo.png';
 
-interface LogoProps extends React.SVGProps<SVGSVGElement> {
+interface LogoProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   size?: number | string;
-  fillColor?: string;
 }
 
-export const Logo: React.FC<LogoProps> = ({ 
-  size = '100%', 
-  fillColor = '#0A84FF', 
+const Logo: React.FC<LogoProps> = ({
+  size = 40,
   className,
-  ...props 
+  style,
+  ...props
 }) => {
   return (
-    <svg
-      viewBox="0 0 100 100"
-      width={size}
-      height={size}
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+    <img
+      src={logo}
+      alt="Lifehut Workspace"
       className={className}
+      style={{
+        width: size,
+        height: size,
+        objectFit: 'contain',
+         transform: 'scale(1.75)',   // Increase to 1.4 or 1.5 if needed
+         transformOrigin: 'center',
+        ...style,
+      }}
       {...props}
-    >
-      {/* Centered, high-fidelity sky blue crescent moon matching the uploaded design */}
-      <path
-        d="M 23.1,12.5 Q 2.4,51.5 81.2,87.5 Q 26.1,69.9 23.1,12.5 Z"
-        fill={fillColor}
-      />
-    </svg>
+    />
   );
 };
 
 export default Logo;
+
